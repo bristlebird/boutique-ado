45e7ea79de1cb566c4cecdbaecd976d1c9b212b7
Adam Green <adam@bristlebird.com>
2025-02-01 00:36:56 +0000

    added profile form, views and updated template

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -39,10 +39,6 @@ class StripeWH_Handler:
         shipping_details = intent.shipping
         grand_total = round(stripe_charge.amount / 100, 2) # updated
 
-        # billing_details = intent.charges.data[0].billing_details
-        # shipping_details = intent.shipping
-        # grand_total = round(intent.charges.data[0].amount / 100, 2)
-
         # Clean data in the shipping details
         for field, value in shipping_details.address.items():
             if value == "":
