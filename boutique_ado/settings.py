f4b08f2216e636ee8e77ce421665e9d03ccec45f
Adam Green <adam@bristlebird.com>
2025-02-03 11:29:50 +0000

    056 prep for deployment part 2

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 # import environ  # new
 import os
+import dj_database_url
 from pathlib import Path
 if os.path.isfile('env.py'):
     import env
@@ -36,6 +37,7 @@ DEBUG = (bool(int(os.environ.get('DEBUG_VALUE', 0))))
 
 
 ALLOWED_HOSTS = [
+    'bristlebird-boutique-ado.herokuapp.com', 
     '127.0.0.1',
     'locahost',
     ]
@@ -134,12 +136,19 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 
 
 # Password validation
